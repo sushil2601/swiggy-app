ae1e55e6a3020fd305d3516b2862729462ceede4
sushil2601 <sushilsuman2355@gmail.com>
2024-12-30 23:51:12 +0530

    nested structure

--- App.js
+++ App.js
@@ -1,8 +1,15 @@
 const heading = React.createElement('h1',{id:'heading'},'Hello World using React')
 
-
-console.log(heading)  //object
+const parent = React.createElement(
+    'div',
+    {id:'parent'},
+    React.createElement(
+        'div',
+        {id:'child'},
+        React.createElement('h1',{},"I'm inside h1 tag")
+    )
+)
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
-root.render(heading)
+root.render(parent) // 
