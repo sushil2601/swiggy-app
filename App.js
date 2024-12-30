9b78aaa629d48bc9632cde022c0670837d2aedaa
sushil2601 <sushilsuman2355@gmail.com>
2024-12-31 00:02:47 +0530

    creating sibling

--- App.js
+++ App.js
@@ -3,12 +3,22 @@ const heading = React.createElement('h1',{id:'heading'},'Hello World using React
 const parent = React.createElement(
     'div',
     {id:'parent'},
+    [
+        React.createElement(
+            'div',
+            {id:'child1'},
+            [React.createElement('h1',{},"I'm inside h1 tag"),React.createElement('h2',{},"I'm inside h2 tag")]
+        ),
+    
     React.createElement(
         'div',
-        {id:'child'},
-        React.createElement('h1',{},"I'm inside h1 tag")
-    )
+        {id:'child2'},
+        [React.createElement('h1',{},"I'm inside h1 tag"),React.createElement('h2',{},"I'm inside h2 tag")]
+    )]
 )
+
+//need of jsx
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
