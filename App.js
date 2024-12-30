f321e1d0e7ef40c85706daeaa1d91c37110582e0
sushil2601 <sushilsuman2355@gmail.com>
2024-12-30 23:37:52 +0530

    created app.js

---
+++ App.js
@@ -0,0 +1,8 @@
+const heading = React.createElement('h1',{id:'heading'},'Hello World using React')
+
+
+console.log(heading)  //object
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(heading)
